2d031d88a91252a1ec73acf174b61107e14b5c0f
KjOHnBaggins <okenshieldak47@gmail.com>
2023-02-13 11:11:51 +0630

    setup

---
+++ README.md
@@ -0,0 +1,3 @@
+#reminder
+
+- app/store.ts, there's thunkaction you left off
---
+++ src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div className="App">The setting sun the rising sun</div>;
+}
+
+export default App;
